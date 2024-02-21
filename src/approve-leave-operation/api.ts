b8e2b1c858d46c0cbe47efab296499611b1f20db
Craig Harman <craigharman@transdigital.com.au>
2024-02-21 16:57:41 +0800

    feat: :sparkles: Email an address when leave approved

--- src/approve-leave-operation/api.ts
+++ src/approve-leave-operation/api.ts
@@ -1,5 +1,7 @@
 import { createError } from "@directus/errors"
 import { defineOperationApi } from "@directus/extensions-sdk"
+import { ref } from "vue"
+import { useItems } from "@directus/extensions-sdk"
 
 type Options = {
 	// 	text: string
@@ -34,9 +36,15 @@ const UnknownError = createError(
 	400
 )
 
+const EmailError = createError(
+	"EMAIL_ERROR",
+	"Failed to send email, check email settings.",
+	400
+)
+
 export default defineOperationApi<Options>({
 	id: "ts_approve_leave",
-	handler: async ({}, { data, services, accountability, getSchema }) => {
+	handler: async ({}, { data, services, accountability, getSchema, env }) => {
 		let leaveIds = []
 		let collection = undefined
 
@@ -59,8 +67,10 @@ export default defineOperationApi<Options>({
 			throw new LeaveCollectionError()
 		}
 
+		const schema = await getSchema()
+
 		const leaveService = new services.ItemsService("ts_leave", {
-			schema: await getSchema(),
+			schema,
 			accountability: accountability,
 		})
 
@@ -73,6 +83,58 @@ export default defineOperationApi<Options>({
 			})
 		} catch (err) {
 			throw new UnknownError()
+		}
+
+		// If ts_settings.leave_notification is not null, then we need to email that address with all the approved leave
+		const settingsService = new services.ItemsService("ts_settings", {
+			schema: await getSchema(),
+			accountability,
+		})
+		const settings = await settingsService.readSingleton({
+			fields: ["leave_notification"],
+		})
+
+		if (settings.leave_notification !== null) {
+			// Get all the approved leave
+			const leaveService = new services.ItemsService("ts_leave", {
+				schema: await getSchema(),
+				accountability,
+			})
+			const approvedLeave = await leaveService.readMany(leaveIds, {
+				fields: [
+					"user.first_name",
+					"user.last_name",
+					"start_date",
+					"end_date",
+					"leave_type.name",
+				],
+				filter: {
+					leave_type: {
+						_in: [1, 2, 5], // TODO: Remove hard coded leave type IDs in favour of something configurable
+					},
+				},
+			})
+
+			// Send email
+			const { MailService } = services
+			const mailService = new MailService({ schema })
+
+			mailService
+				.send({
+					to: [settings.leave_notification],
+					from: env.EMAIL_FROM,
+					subject: "Leave approved",
+					template: {
+						name: "leave-approved",
+						data: {
+							approvedLeave,
+						},
+					},
+				})
+				.catch((err: any) => {
+					console.error(err)
+					throw new EmailError()
+				})
 		}
 	},
 })
